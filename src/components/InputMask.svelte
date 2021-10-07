4e7d615035564c3a0e1f7ecab1d580c10f29e8ea
10c8 <2660859+10c8@users.noreply.github.com>
2021-10-07 16:27:30 -0300

    Changed the input mask implementation

---
+++ src/components/InputMask.svelte
@@ -0,0 +1,68 @@
+<script>
+  import { onMount, onDestroy, tick } from 'svelte';
+  import IMaskSvelte from '@imask/svelte';
+  import IMask from 'imask';
+
+  export let value;
+
+	const action = IMaskSvelte.imask;
+
+  let input;
+  let maskRef, imask, unmask, attrs;
+
+  $: {
+    ({ imask, unmask, ...attrs } = $$props);
+
+    if (maskRef) {
+      writeValue(value);
+      attrs.value = maskRef.value;
+      tick().then(() => value = getValue());
+    }
+  }
+
+  function getValue() {
+    if (unmask === 'typed') return maskRef.typedValue;
+    if (unmask) return maskRef.unmaskedValue;
+    return maskRef.value;
+  }
+
+  function setValue(v) {
+    v = v == null ? '' : v;
+    if (unmask === 'typed') maskRef.typedValue = v;
+    else if (unmask) maskRef.unmaskedValue = v;
+    else maskRef.value = v;
+  }
+
+  function writeValue(v) {
+    if (getValue() !== v ||
+      // handle cases like Number('') === 0,
+      // for details see https://github.com/uNmAnNeR/imaskjs/issues/134
+      (typeof v !== 'string' && value === '') && !maskRef.el.isActive
+    ) {
+      setValue(v);
+    }
+  }
+
+  onMount(() => {
+    maskRef = IMask(input, imask);
+    setValue(value);
+  });
+
+  onDestroy(() => {
+    if (maskRef) maskRef.destroy();
+    maskRef = undefined;
+  });
+
+  function accept({ detail: mask }) {
+    value = getValue();
+  }
+</script>
+
+<input type="text"
+  bind:this={input}
+  use:action={maskRef}
+  {...attrs}
+  on:accept={accept}
+  on:accept
+  on:complete
+/>
--- src/components/ProductEditor.svelte
+++ src/components/ProductEditor.svelte
@@ -1,19 +1,23 @@
 <script>
+  import { getContext, onMount } from 'svelte';
   import firebase from 'firebase/app';
+  import 'firebase/firestore';
   import 'firebase/storage';
-  import { imask } from '@imask/svelte';
+  import { Collection } from 'sveltefire';
+
+  import InputMask from '@/components/InputMask.svelte';
 
   export let isCreating = false;
   export let id;
 
   let values = {
     name: '',
-    price: 0.0,
+    price: '0',
     image: '',
     stock: 0,
-    category: ''
+    category: null
   };
-
+  let categoryTitle = 'Selecione uma categoria';
   let imageIsUploading = false;
   let imageFile;
 
@@ -34,11 +38,43 @@
     max: 9999999.99,
   };
 
-  // Helpers
-  const accept = fieldName => ({ detail: maskRef }) => {
-    values[fieldName] = maskRef.value;
-  };
+  const { close } = getContext('simple-modal');
 
+  onMount(() => {
+    const db = firebase.firestore();
+
+    // If we're editing an existing product, fetch its data
+    if (!isCreating && id) {
+      db.collection('products')
+        .doc(id)
+        .get()
+        .then(doc => {
+          values = doc.data();
+
+          // Fetch category title
+          db
+            .collection('categories')
+            .doc(values.category.toString())
+            .get()
+            .then(doc => {
+              categoryTitle = doc.data().title;
+            })
+            .catch(err => {
+              console.error(err);
+            });
+        });
+    };
+
+    // Handle image uploading
+    imageFile.addEventListener('change', () => {
+      if (imageFile.files.length === 0)
+        return;
+
+      doImageUpload();
+    });
+  });
+
+  // Helpers
   const decreaseStock = () => {
     values.stock--;
   };
@@ -57,6 +93,11 @@
       values.stock = 99999;
   };
 
+  const setCategory = (id, title) => {
+    values.category = id;
+    categoryTitle = title;
+  };
+
   const handleImageClick = () => {
     if (imageIsUploading)
       return;
@@ -64,13 +105,18 @@
     imageFile.click();
   };
 
-  const handleImageChange = () => {
+  const doImageUpload = () => {
     if (imageFile.files.length === 0)
       return;
 
+    const storage = firebase.storage();
+
     if (values.image !== '') {
       // Delete the old image from the storage first
-      firebase.storage().refFromURL(values.image).delete();
+      storage
+        .refFromURL(values.image)
+        .delete();
+
       values.image = '';
     }
 
@@ -78,9 +124,8 @@
     imageIsUploading = true;
 
     const file = imageFile.files[0];
-
-    const storageRef = firebase.storage().ref();
-    const productImagesRef = storageRef
+    const productImagesRef = storage
+      .ref()
       .child(`images/products/${file.name}`);
 
     productImagesRef
@@ -92,40 +137,93 @@
   }
 
   const handleSubmit = () => {
-    console.log(`Name: ${values.name}`);
-    console.log(`Price: ${values.price}`);
-    console.log(`Image: ${values.image}`);
-    console.log(`Stock: ${values.stock}`);
-    console.log(`Category: ${values.category}`);
+    const db = firebase.firestore();
+
+    if (isCreating) {
+      // Create a product document on firestore
+      const productRef = db
+        .collection('products')
+        .doc();
+
+      productRef
+        .set({
+          name: values.name,
+          price: values.price,
+          image: values.image,
+          stock: values.stock,
+          category: values.category
+        })
+        .then(() => {
+          close();
+        })
+        .catch(err => {
+          alert(`Erro ao criar produto: ${err}`);
+        });
+    } else {
+      // Update the product document on firestore
+      db
+        .collection('products')
+        .doc(id)
+        .update({
+          name: values.name,
+          price: values.price,
+          image: values.image,
+          stock: values.stock,
+          category: values.category,
+          created_in: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .then(() => {
+          close();
+        })
+        .catch(err => {
+          alert(`Erro ao atualizar produto: ${err}`);
+        });
+    }
   };
+
+  const handleDelete = () => {
+    if (!confirm('Tem certeza que deseja excluir este produto?'))
+      return;
+
+    // Delete the product document from firestore
+    firebase.firestore()
+      .collection('products')
+      .doc(id)
+      .delete()
+      .then(() => {
+        close();
+      });
+  };
+
 </script>
 
-<div class="flex items-center w-full pt-8">
+<div class="flex items-center w-full pt-8 {!isCreating && 'pb-12'}">
   <div class="grid w-full grid-cols-4 gap-4 px-4 pb-4">
     <!-- Name -->
     <div class="col-span-full">
       <label for="name" class="label">Nome</label>
-      <input
+      <InputMask
         type="text"
         name="name"
-        autofocus={isCreating}
-        class="flex items-center w-full h-10 px-4 mt-1 text-sm border rounded"
-        use:imask={productNameOptions}
-        on:accept={accept('name')}
+        class="flex items-center w-full h-10 px-4 mt-1 text-sm border-2 rounded"
+        unmask="typed"
+        imask={productNameOptions}
+        bind:value={values.name}
       />
     </div>
 
     <!-- Price -->
     <div class="col-span-full md:col-span-2">
       <label for="price" class="label">Preço</label>
-      <div class="box-border flex items-center w-full h-10 gap-2 px-4 mt-1 overflow-hidden text-sm border rounded focus-within:border-black focus-within:border-2 focus-within:px-[15px]">
+      <div class="flex items-center w-full h-10 gap-2 px-4 mt-1 overflow-hidden text-sm border-2 rounded focus-within:border-black focus-within:border-2 focus-within:px-[15px]">
         <span class="leading-4 select-none">R$</span>
-        <input
+        <InputMask
           type="text"
           name="price"
           class="w-full py-3 leading-4 focus:outline-none"
-          use:imask={productPriceOptions}
-          on:accept={accept('price')}
+          unmask="typed"
+          imask={productPriceOptions}
+          bind:value={values.price}
         />
       </div>
     </div>
@@ -135,7 +233,7 @@
       <label for="stock" class="label">Estoque</label>
       <div class="flex items-center justify-between w-full h-10 gap-2 mt-1">
         <button
-          class="w-10 button hover:bg-blue-600 active:bg-blue-400"
+          class="w-10 bg-blue-500 button hover:bg-blue-600 active:bg-blue-400"
           title="Diminuir"
           on:click={decreaseStock}
           disabled={values.stock === 0 || values.stock === null}
@@ -147,18 +245,45 @@
         <input
           type="number"
           name="stock"
-          class="flex items-center w-full h-10 px-4 text-sm text-center border rounded"
+          class="flex items-center w-full h-10 px-4 text-sm text-center border-2 rounded"
           bind:value={values.stock}
           on:change={handleStockChange}
         />
         <button
-          class="w-10 button hover:bg-blue-600 active:bg-blue-400"
+          class="w-10 bg-blue-500 button hover:bg-blue-600 active:bg-blue-400"
           title="Aumentar"
           on:click={increaseStock}
         >
           <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
           </svg>
+        </button>
+      </div>
+    </div>
+
+    <!-- Category -->
+    <div class="col-span-full">
+      <label for="category" class="label">Categoria</label>
+      <div class="flex items-center justify-between w-full h-10 gap-2 mt-1">
+        <button class="relative w-full text-sm focus:outline-none group sm:mt-0">
+          <div class="flex items-center justify-between h-10 px-3 text-gray-700 border-2 rounded hover:bg-gray-200">
+            <span class="text-sm">{categoryTitle}</span>
+            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
+          </div>
+          <div class="absolute z-10 flex-col items-start hidden w-full pb-1 bg-white rounded shadow-lg group-focus:flex">
+            <Collection path={'categories'} let:data={categories}>
+              {#each categories as category, i (i)}
+                <button
+                  class="w-full px-4 py-2 text-left hover:bg-gray-200"
+                  on:click={() => setCategory(i, category.title)}
+                >
+                  {category.title}
+                </button>
+              {/each}
+            </Collection>
+          </div>
         </button>
       </div>
     </div>
@@ -172,7 +297,6 @@
         id="image_file"
         accept="image/png,image/jpeg,image/webp"
         bind:this={imageFile}
-        on:change={handleImageChange}
         hidden
       />
       {#if values.image === ''}
@@ -213,27 +337,41 @@
     </div>
     <div class="h-10 col-span-4 mt-3">
       <button
-        class="w-full font-medium button hover:bg-blue-600 active:bg-blue-400"
+        class="w-full font-medium bg-blue-500 button hover:bg-blue-600 active:bg-blue-400"
         on:click={handleSubmit}
       >
         {isCreating ? 'Criar Produto' : 'Salvar Alterações'}
       </button>
+      {#if !isCreating}
+        <button
+          class="w-full mt-2 font-medium bg-red-500 button hover:bg-red-600 active:bg-red-400"
+          on:click={handleDelete}
+        >
+          Excluir Produto
+        </button>
+      {/if}
     </div>
   </div>
 </div>
 
 <style scoped>
+  .group:focus .group-focus\:flex,
+  .group-focus\:flex:active {
+    display: flex;
+  }
+
   .label {
     @apply text-xs font-semibold select-none;
   }
 
   .button {
-    @apply flex items-center justify-center flex-shrink-0 h-full text-sm text-white bg-blue-500 rounded;
+    @apply flex items-center justify-center flex-shrink-0 h-full text-sm text-white rounded;
   }
 
   .button:disabled {
     @apply bg-blue-300;
   }
+
   input::-webkit-outer-spin-button,
   input::-webkit-inner-spin-button {
     -webkit-appearance: none;

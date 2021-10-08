fb05068d1e73f37c9784bb1748000483d3ddb51a
10c8 <2660859+10c8@users.noreply.github.com>
2021-10-08 13:38:16 -0300

    Implemented category searching

--- src/pages/admin/Categories.svelte
+++ src/pages/admin/Categories.svelte
@@ -131,40 +131,42 @@
       <tbody class="flex flex-col space-y-3 flex-rol">
         <Collection path={'categories'} let:data={categories}>
           {#each categories as category, i (i)}
-            <tr class="flex w-full bg-white rounded-lg shadow-md space-between">
-              <td class="flex items-center flex-shrink-0 w-20 col-span-3 p-3 md:flex-shrink md:w-full">
-                <span class="ml-3">{category.title}</span>
-              </td>
-              <td class="flex items-center justify-center w-20 p-3 ml-auto md:flex-shrink-0 md:justify-start">
-                {#await getItemCount(i)}
-                  ...
-                {:then count}
-                  {count}
-                {:catch}
-                  ???
-                {/await}
-              </td>
-              <td class="flex items-center justify-center w-40 p-3 space-x-3 md:flex-shrink-0">
-                <button
-                  title="Editar"
-                  class="flex items-center justify-center p-2 bg-blue-500 rounded text-blue-50 hover:bg-blue-600 active:bg-blue-600"
-                  on:click={() => showCategoryEditor(false, category.id)}
-                >
+            {#if currentSearchTerm === '' || category.title.toLowerCase().includes(currentSearchTerm.toLowerCase())}
+              <tr class="flex w-full bg-white rounded-lg shadow-md space-between">
+                <td class="flex items-center flex-shrink-0 w-20 col-span-3 p-3 md:flex-shrink md:w-full">
+                  <span class="ml-3">{category.title}</span>
+                </td>
+                <td class="flex items-center justify-center w-20 p-3 ml-auto md:flex-shrink-0 md:justify-start">
+                  {#await getItemCount(i)}
+                    ...
+                  {:then count}
+                    {count}
+                  {:catch}
+                    ???
+                  {/await}
+                </td>
+                <td class="flex items-center justify-center w-40 p-3 space-x-3 md:flex-shrink-0">
+                  <button
+                    title="Editar"
+                    class="flex items-center justify-center p-2 bg-blue-500 rounded text-blue-50 hover:bg-blue-600 active:bg-blue-600"
+                    on:click={() => showCategoryEditor(false, category.id)}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    </svg>
+                  </button>
+                  <button
+                    title="Excluir"
+                    class="flex items-center justify-center p-2 bg-red-500 rounded text-red-50 hover:bg-red-600 active:bg-red-600"
+                    on:click={() => deleteCategory(category.id, category.title)}
+                  >
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                   </svg>
-                </button>
-                <button
-                  title="Excluir"
-                  class="flex items-center justify-center p-2 bg-red-500 rounded text-red-50 hover:bg-red-600 active:bg-red-600"
-                  on:click={() => deleteCategory(category.id, category.title)}
-                >
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg>
-                </button>
-              </td>
-            </tr>
+                  </button>
+                </td>
+              </tr>
+            {/if}
           {/each}
         </Collection>
       </tbody>

1ad6cd22a71456571e79f8f3b8f439dbbc9e563a
10c8 <2660859+10c8@users.noreply.github.com>
2021-10-13 18:33:05 -0300

    Implemented estimate list

---
+++ src/pages/admin/Estimates.svelte
@@ -0,0 +1,208 @@
+<svelte:head>
+  <title>Admin - Orçamentos</title>
+</svelte:head>
+
+<script>
+  import { navigateTo } from 'svelte-router-spa';
+  import firebase from 'firebase/app';
+  import 'firebase/firestore';
+  import { Collection } from 'sveltefire';
+
+  let currentSearchTerm = '';
+
+  // Helpers
+  const formatDate = timestamp => {
+    const date = new Date(timestamp.seconds * 1000);
+    return date.toLocaleDateString('pt-BR');
+  };
+
+  const filterSearch = (items, term) => {
+    if (term === '')
+      return items;
+
+    return items.filter(item => {
+      const title = item.title.toLowerCase();
+      const searchTerm = term.toLowerCase();
+
+      return title.includes(searchTerm);
+    });
+  };
+
+  const openEstimateEditor = id => {
+    navigateTo(`/admin/editor?id=${id}`);
+  };
+
+
+  const openEstimatePreview = id => {
+    window.open(`/estimate-preview?id=${id}`, '_blank').focus();
+  };
+
+  const deleteEstimate = id => {
+    if (!confirm(`Tem certeza que deseja excluir o orçamento?`))
+      return;
+
+    const db = firebase.firestore();
+
+    db
+      .collection('estimates')
+      .doc(id)
+      .delete()
+      .catch(error => {
+        alert(`Erro ao excluir o orçamento: ${error.message}`);
+      });
+  };
+</script>
+
+<main class="flex flex-col w-screen min-h-screen p-4 pt-8 overflow-y-auto text-gray-800 bg-gray-50 md:p-8">
+  <!-- Header -->
+  <h1 class="text-3xl">Orçamentos</h1>
+
+  <!-- Table Options -->
+  <div class="flex flex-col mt-6 lg:flex-row lg:items-end lg:justify-between">
+    <!-- Search -->
+    <div class="relative w-full mt-4 text-sm lg:w-auto lg:mt-0">
+      <div class="flex items-center justify-between w-full h-10 border-2 border-gray-300 rounded lg:w-50 focus-within:border-2 focus-within:border-black">
+        <input
+          class="flex items-center w-full h-full px-4 bg-gray-100 rounded focus:outline-none"
+          type="text"
+          placeholder="Pesquisar por Cliente"
+          bind:value={currentSearchTerm}
+        />
+        {#if currentSearchTerm === ''}
+          <div class="flex items-center justify-center px-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </div>
+        {:else}
+          <button
+            class="flex items-center justify-center px-2"
+            on:click={() => currentSearchTerm = ''}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        {/if}
+      </div>
+    </div>
+  </div>
+
+  <!-- Table -->
+  <div class="flex flex-col w-full mt-6">
+    <table class="flex flex-col space-y-3 text-sm text-gray-700">
+      <thead class="sticky text-gray-700 bg-white rounded-lg shadow-lg top-[-15px] uppercase">
+        <tr class="flex justify-between">
+          <th class="w-20 p-3 ml-3 text-left md:w-full">
+            Nome do Cliente
+          </th>
+          <th class="w-20 p-3 ml-3 text-left md:w-full">
+            Data
+          </th>
+          <th class="flex-shrink-0 w-20 py-3 text-left md:ml-auto md:p-3 md:text-center md:w-40">
+            Ações
+          </th>
+        </tr>
+      </thead>
+      <tbody class="flex flex-col space-y-3 flex-rol">
+        <Collection
+          path={'estimates'}
+          query={ref => ref.orderBy('updated_in', 'desc')}
+          let:data={estimates}
+        >
+          <div slot="loading" class="flex items-center justify-center w-full h-full mt-10">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 text-gray-500 animate-bounce" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10" />
+            </svg>
+          </div>
+
+          <div slot="fallback" class="flex flex-col items-center justify-center w-full h-full mt-10 space-y-3 text-gray-500">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span class="font-semibold text-md">Erro ao carregar dados!</span>
+          </div>
+
+          {#if filterSearch(estimates, currentSearchTerm).length === 0}
+            <div class="flex flex-col items-center justify-center w-full h-full mt-10 space-y-3 text-gray-500">
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span class="font-semibold text-md">
+                Nenhum orçamento encontrado.
+              </span>
+            </div>
+          {/if}
+
+          {#each filterSearch(estimates, currentSearchTerm) as estimate, i (i)}
+            <tr class="flex w-full bg-white rounded-lg shadow-md space-between">
+              <td class="flex items-center flex-shrink-0 w-20 col-span-3 p-3 md:flex-shrink md:w-full">
+                <span class="ml-3 text-sm font-semibold">
+                  {estimate.client.name}
+                </span>
+              </td>
+              <td class="flex items-center flex-shrink-0 w-20 col-span-3 p-3 md:flex-shrink md:w-full">
+                <span class="ml-3 text-sm font-semibold">
+                  {formatDate(estimate.created_in)}
+                </span>
+              </td>
+              <td class="flex items-center justify-center w-20 p-3 ml-auto md:space-x-3 md:flex-shrink-0 md:w-40">
+                <button
+                  title="Visualizar"
+                  class="flex items-center justify-center p-2 text-blue-500 rounded hover:bg-blue-200 active:bg-blue-100"
+                  on:click={() => openEstimatePreview(estimate.id)}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                  </svg>
+                </button>
+                <button
+                  title="Editar"
+                  class="flex items-center justify-center p-2 text-blue-500 rounded hover:bg-blue-200 active:bg-blue-100"
+                  on:click={() => openEstimateEditor(estimate.id)}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                  </svg>
+                </button>
+                <button
+                  title="Excluir"
+                  class="flex items-center justify-center p-2 text-red-500 rounded hover:bg-red-200 active:bg-red-100"
+                  on:click={() => deleteEstimate(estimate.id)}
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+                </button>
+              </td>
+            </tr>
+          {/each}
+        </Collection>
+      </tbody>
+    </table>
+  </div>
+</main>
+
+<style scoped>
+  table {
+    border-spacing: 0 15px;
+  }
+
+  table:after {
+    content: '';
+    display: block;
+    height: 35px;
+    width: 100%;
+  }
+
+  tr td:first-child,
+	tr th:first-child {
+    @apply rounded-l-lg;
+	}
+
+  tr td:last-child,
+	tr th:last-child {
+    @apply rounded-r-lg;
+	}
+</style>
